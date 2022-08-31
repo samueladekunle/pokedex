512ba728325e19225b7e6a6a6496f10bbc77ca1c
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-08-31 11:53:25 +0100

    Add loading indicator widget

---
+++ lib/src/core/widgets/indicator.dart
@@ -0,0 +1,43 @@
+import 'dart:math' show pi;
+
+import 'package:flutter/material.dart';
+import 'package:pokedex/gen/assets.gen.dart';
+
+class LoadingIndicator extends StatefulWidget {
+  const LoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingIndicator> createState() => _LoadingIndicatorState();
+}
+
+class _LoadingIndicatorState extends State<LoadingIndicator>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 800),
+  )..repeat();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: controller,
+        builder: (_, child) {
+          return Transform.rotate(
+            angle: controller.value * 2 * pi,
+            child: child,
+          );
+        },
+        child: Assets.images.appIcon.image(
+          height: 36,
+        ),
+      ),
+    );
+  }
+}
