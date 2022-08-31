94eb8eb7096899fc5c8975eea4c1719268cee2a7
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-08-31 11:31:57 +0100

    Add hive config

---
+++ lib/src/config/hive_config.dart
@@ -0,0 +1,22 @@
+class HiveTypeId {
+  const HiveTypeId._();
+
+  ///
+  static const resource = 0;
+
+  ///
+  static const pokemon = 1;
+
+  ///
+  static const pokemonStat = 2;
+
+  ///
+  static const pokemonType = 3;
+}
+
+class HiveBoxName {
+  const HiveBoxName._();
+
+  ///
+  static const favouritePokemon = 'FAVOURITE_POKEMON';
+}
